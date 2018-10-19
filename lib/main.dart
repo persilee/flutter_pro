70d90fbaa6b59356eb6757f5ce33a584da35625e
persilee <i@lishaoy.net>
2018-10-19 20:14:07 +0800

    add listView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
 void main() {
   runApp(
@@ -8,18 +9,51 @@ void main() {
 
 class App extends StatelessWidget {
   @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+        primaryColor: Colors.yellow
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 12.0,),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 12.0,)
+        ],
+      ),
+    );
+  }
+  @override
     Widget build(BuildContext context) {
       // TODO: implement build
-      return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Lishaoy.net'),
-            elevation: 6.0,
-          ),
-          body: Hello(),
+      return Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('Lishaoy.net'),
+          elevation: 6.0,
         ),
-        theme: ThemeData(
-          primaryColor: Colors.yellow
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
         ),
       );
     }
@@ -27,18 +61,18 @@ class App extends StatelessWidget {
 
 class Hello extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return Center(
-        child: Text(
-          'holle',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 40.0,
-            color: Colors.black87,
-          ),
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        'holle',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 40.0,
+          color: Colors.black87,
         ),
-      );
-    }
+      ),
+    );
+  }
 }
