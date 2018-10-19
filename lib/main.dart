acd1eb19b6fc8911d0dd206c5be63467424d2a39
persilee <i@lishaoy.net>
2018-10-19 19:41:56 +0800

    add post.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: 'Flutter Tutorial',
-    home: new TutorialHome(),
-  ));
+  runApp(
+    App()
+  );
 }
 
-class TutorialHome extends StatelessWidget {
+class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    //Scaffold是Material中主要的布局组件.
-    return new Scaffold(
-      appBar: new AppBar(
-        leading: new IconButton(
-          icon: new Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: new Text('lishaoy.net'),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Lishaoy.net'),
+            elevation: 6.0,
           ),
-        ],
-      ),
-      //body占屏幕的大部分
-      body: new Center(
-        child: new Text('Hello, world!'),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: new Icon(Icons.add),
-        onPressed: null,
-      ),
-    );
-  }
+          body: Hello(),
+        ),
+        theme: ThemeData(
+          primaryColor: Colors.yellow
+        ),
+      );
+    }
+}
+
+class Hello extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return Center(
+        child: Text(
+          'holle',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+            color: Colors.black87,
+          ),
+        ),
+      );
+    }
 }
