e2263439748d0217f8003a68021cd2ac494d2d43
persilee <i@lishaoy.net>
2018-10-21 00:31:36 +0800

    add UserAccountsDrawerHeader

--- lib/main.dart
+++ lib/main.dart
@@ -64,23 +64,38 @@ class Home extends StatelessWidget {
           child: ListView(
             padding: EdgeInsets.zero,
             children: <Widget>[
-              DrawerHeader(
-                child: Text('header'),
+              UserAccountsDrawerHeader(
+                accountName: Text('Persilee', style: TextStyle(fontWeight: FontWeight.bold)),
+                accountEmail: Text('i@lishaoy.net'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage('https://cn.gravatar.com/avatar/ebf4749fb999f134566782c20e67a3ac?s=60&d=robohash&r=G'),
+                ),
                 decoration: BoxDecoration(
-                  color: Colors.grey[100]
+                  color: Colors.yellow[400],
+                  image: DecorationImage(
+                    image: NetworkImage('https://resources.ninghao.org/images/childhood-in-a-picture.jpg'),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                      Colors.yellow[400].withOpacity(0.65), 
+                      BlendMode.lighten
+                    )
+                  )
                 ),
               ),
               ListTile(
                 title: Text('Messages', textAlign: TextAlign.right,),
                 trailing: Icon(Icons.message, color: Colors.black12, size: 20.0,),
+                onTap: () => Navigator.pop(context),
               ),
               ListTile(
                 title: Text('Favorite', textAlign: TextAlign.right,),
                 trailing: Icon(Icons.mood, color: Colors.black12, size: 20.0,),
+                onTap: () => Navigator.pop(context),
               ),
               ListTile(
                 title: Text('Settings', textAlign: TextAlign.right,),
                 trailing: Icon(Icons.settings, color: Colors.black12, size: 20.0,),
+                onTap: () => Navigator.pop(context),
               ),
             ],
           ),
