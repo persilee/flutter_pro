bd5dfd9c4bd0571e7076aeda780a85d6a6e7b3c5
persilee <i@lishaoy.net>
2018-10-20 23:12:06 +0800

    add tabBar

--- lib/main.dart
+++ lib/main.dart
@@ -25,25 +25,41 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigration',
-          onPressed: () => debugPrint('Navitration Button is pressed'),
-        ),
-        title: Text('Lishaoy.net'),
-        actions: <Widget>[
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigration',
+            onPressed: () => debugPrint('Navitration Button is pressed'),
+          ),
+          title: Text('Lishaoy.net'),
+          actions: < Widget > [
             IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: () => debugPrint('Search Button is pressed'),
-          )
-        ],
-        elevation: 6.0,
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: () => debugPrint('Search Button is pressed'),
+            )
+          ],
+          elevation: 6.0,
+          bottom: TabBar(
+            tabs: < Widget > [
+              Tab(icon: Icon(Icons.shopping_cart)),
+              Tab(icon: Icon(Icons.spa)),
+              Tab(icon: Icon(Icons.star)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.shopping_cart, size: 126.0, color: Colors.black12,),
+            Icon(Icons.spa, size: 126.0, color: Colors.black12,),
+            Icon(Icons.star, size: 126.0, color: Colors.black12,),
+          ],
+        )
       ),
-      body: null
     );
   }
 }
