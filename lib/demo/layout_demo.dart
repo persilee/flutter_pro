6135647ded870669a2049785623a3ed752f4f4c6
persilee <i@lishaoy.net>
2018-10-23 19:13:10 +0800

    sizedBox

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,7 +3,38 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconBadge(Icons.monetization_on);
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: 300.0,
+            height: 400.0,
+            child: Container(
+              alignment: Alignment(-1.0, -1.0),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(48, 128, 196, 0.6),
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              child: Icon(Icons.opacity, color: Colors.white, size: 36.0,),
+            ),
+          ),
+          SizedBox(height: 36.0, width: 36.0),
+          SizedBox(
+            width: 200.0,
+            height: 200.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(48, 128, 196, 0.6),
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              child: Icon(Icons.ac_unit, color: Colors.white, size: 36.0,),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
