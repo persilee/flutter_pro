a27b11183b5b1365fe496acf397c18cdb97e9c87
persilee <i@lishaoy.net>
2018-10-24 23:51:46 +0800

    add _pageItemBuilder

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+   Widget _pageItemBuilder(BuildContext context, int index) {
+     return Stack(
+        children: <Widget>[
+          SizedBox.expand(
+            child: Image.network(
+              posts[index].imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            bottom: 20.0,
+            left: 20.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  posts[index].title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  posts[index].author,
+                  style: TextStyle(fontWeight: FontWeight.w100),
+                ),
+              ],
+            ),
+          ),
+        ],
+     );
+   }
+
   @override
   Widget build(BuildContext context) {
-    return PageViewDemo();
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder:  _pageItemBuilder,
+    );
   }
 }
 
