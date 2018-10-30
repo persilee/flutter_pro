f0a9b0d47b99b8c625e45c527e624621ed0962d4
persilee <i@lishaoy.net>
2018-10-31 01:23:37 +0800

    add bottomModalSheetDemo

--- lib/demo/bottom_sheet_demo.dart
+++ lib/demo/bottom_sheet_demo.dart
@@ -9,23 +9,52 @@ class _BottomSheetDemoState extends State<BottomSheetDemo> {
   final _bottomSheetDemoState = GlobalKey<ScaffoldState>();
 
   _openBottomSheet() {
-    _bottomSheetDemoState.currentState.showBottomSheet((BuildContext context)  => BottomAppBar(
-      child: Container(
-        height: 100.0,
-        width: double.infinity,
-        padding: EdgeInsets.all(16.0),
-        child: Row(
-          children: <Widget>[
-            Icon(Icons.mic),
-            SizedBox(width: 16.0,),
-            Text('--/--'),
-            Expanded(
-              child: Text('This is bottomSheet',textAlign: TextAlign.right,),
+    _bottomSheetDemoState.currentState
+        .showBottomSheet((BuildContext context) => BottomAppBar(
+              child: Container(
+                height: 100.0,
+                width: double.infinity,
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.mic),
+                    SizedBox(
+                      width: 16.0,
+                    ),
+                    Text('--/--'),
+                    Expanded(
+                      child: Text(
+                        'This is bottomSheet',
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ));
+  }
+
+  _openModalBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 200.0,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('Option A'),
+                ),
+                ListTile(
+                  title: Text('Option B'),
+                ),
+                ListTile(
+                  title: Text('Option C'),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    ));
+          );
+        });
   }
 
   @override
@@ -48,6 +77,12 @@ class _BottomSheetDemoState extends State<BottomSheetDemo> {
                   icon: Icon(Icons.missed_video_call),
                   label: Text('BottomSheet'),
                   onPressed: _openBottomSheet,
+                  splashColor: Colors.grey[200],
+                ),
+                FlatButton.icon(
+                  icon: Icon(Icons.more_horiz),
+                  label: Text('ModalBottomSheet'),
+                  onPressed: _openModalBottomSheet,
                   splashColor: Colors.grey[200],
                 ),
               ],
