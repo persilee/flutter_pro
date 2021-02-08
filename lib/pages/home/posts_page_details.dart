b6bbf10cce973748512f55dea68a10ad8e1023d9
persilee <i@lishaoy.net>
2021-02-08 08:56:22 +0800

    🍉 add scroll for details page

--- lib/pages/home/posts_page_details.dart
+++ lib/pages/home/posts_page_details.dart
@@ -17,28 +17,64 @@ final postsDetailsProvider = StateNotifierProvider.autoDispose
   return DetailsViewModel(postId);
 });
 
-class PostsPageDetails extends ConsumerWidget {
+class PostsPageDetails extends StatefulWidget {
   final int postId;
-  final ScrollController _scrollController = ScrollController();
 
   PostsPageDetails({@required this.postId});
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  _PostsPageDetailsState createState() => _PostsPageDetailsState();
+}
+
+class _PostsPageDetailsState extends State<PostsPageDetails> with WidgetsBindingObserver {
+  double imageHeight;
+  bool isShowBottomBar = true;
+  Duration duration = Duration(milliseconds: 360);
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      print(_scrollController.position.pixels);
+      if (_scrollController.position.pixels > imageHeight - 56) {
+        setState(() {
+          isShowBottomBar = false;
+        });
+      } else {
+        setState(() {
+          isShowBottomBar = true;
+        });
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     StatusBarUtil.setStatusBar(Brightness.light);
-    final detailsState = watch(postsDetailsProvider(postId).state);
+
     return Scaffold(
-      body: _createContent(detailsState, context),
+      body: Consumer(builder: (context, watch, _) {
+        final detailsState = watch(postsDetailsProvider(widget.postId).state);
+        return _createContent(detailsState, context);
+      }),
     );
   }
 
   Widget _createContent(DetailsState detailsState, BuildContext context) {
     final post = detailsState?.post;
     final size = MediaQuery.of(context).size;
-    final imageHeight = post?.coverImage != null
+    imageHeight = post?.coverImage != null
         ? post.coverImage.height / (post.coverImage.width / size.width)
         : 0;
-
     if (detailsState.pageState == PageState.busyState ||
         detailsState.pageState == PageState.initializedState) {
       return Center(
@@ -51,105 +87,111 @@ class PostsPageDetails extends ConsumerWidget {
       );
     }
 
-    return Stack(
-      children: [
-        SingleChildScrollView(
-          controller: _scrollController,
-          child: Column(
-            children: [
-              Stack(
-                overflow: Overflow.visible,
-                children: [
-                  _createCoverImage(post),
-                  _createAppBar(size, context),
-                  _createBackdropFilter(imageHeight, post, size),
-                ],
-              ),
-              _createText(post),
-              _createImage(post),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 20,
-          child: Container(
-            padding: EdgeInsets.only(left: 26, right: 26),
-            width: size.width,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+    return Container(
+      height: size.height,
+      child: Stack(
+        children: [
+          SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
               children: [
-                Container(
-                  padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black87.withOpacity(0.1),
-                        blurRadius: 8.0,
-                        spreadRadius: 1,
-                      ),
-                    ],
-                    color: Color.fromRGBO(249, 249, 249, 1),
-                    border: Border.all(color: Colors.white.withOpacity(0.6), width: 1.0),
-                    borderRadius: BorderRadius.all(Radius.circular(36)),
-                  ),
-                  child: Icon(
-                    IconFont.icon_fenxiang,
-                    size: 22,
-                    color: Colors.black87,
-                  ),
+                Stack(
+                  overflow: Overflow.visible,
+                  children: [
+                    _createCoverImage(post),
+                    _createBackdropFilter(imageHeight, post, size),
+                  ],
                 ),
-                Spacer(),
-                Container(
-                  padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black87.withOpacity(0.1),
-                        blurRadius: 8.0,
-                        spreadRadius: 1,
-                      ),
-                    ],
-                    color: Color.fromRGBO(249, 249, 249, 1),
-                    border: Border.all(color: Colors.white.withOpacity(0.6), width: 1.0),
-                    borderRadius: BorderRadius.all(Radius.circular(36)),
-                  ),
-                  child: Icon(
-                    IconFont.icon_message,
-                    size: 22,
-                    color: Colors.black87,
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(right: 10)),
-                IconAnimationWidget(
-                  icon: Container(
-                    padding: EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                              BoxShadow(
-                                color: Colors.black87.withOpacity(0.1),
-                                blurRadius: 8.0,
-                                spreadRadius: 1,
-                              ),
-                            ],
-                      color: Color.fromRGBO(249, 249, 249, 1),
-                      border: Border.all(color: Colors.white.withOpacity(0.6), width: 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(36)),
-                    ),
-                    child: Icon(
-                      Icons.favorite,
-                      size: 22,
-                      color: post?.liked == 0
-                          ? Colors.red.withOpacity(0.9)
-                          : Colors.grey.withOpacity(0.8),
-                    ),
-                  ),
-                  clickCallback: () async {},
-                ),
+                _createText(post),
+                _createImage(post),
               ],
             ),
           ),
-        ),
-      ],
+          isShowBottomBar ? _createAppBar(size, context, isShowBottomBar) : _createLightAppBar(size, context, post),
+          Positioned(
+            bottom: 20,
+            child: Container(
+              padding: EdgeInsets.only(left: 26, right: 26),
+              width: size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black87.withOpacity(0.1),
+                          blurRadius: 8.0,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                      color: Color.fromRGBO(249, 249, 249, 1),
+                      border: Border.all(
+                          color: Colors.white.withOpacity(0.6), width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(36)),
+                    ),
+                    child: Icon(
+                      IconFont.icon_fenxiang,
+                      size: 22,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black87.withOpacity(0.1),
+                          blurRadius: 8.0,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                      color: Color.fromRGBO(249, 249, 249, 1),
+                      border: Border.all(
+                          color: Colors.white.withOpacity(0.6), width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(36)),
+                    ),
+                    child: Icon(
+                      IconFont.icon_message,
+                      size: 22,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 10)),
+                  IconAnimationWidget(
+                    icon: Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black87.withOpacity(0.1),
+                            blurRadius: 8.0,
+                            spreadRadius: 1,
+                          ),
+                        ],
+                        color: Color.fromRGBO(249, 249, 249, 1),
+                        border: Border.all(
+                            color: Colors.white.withOpacity(0.6), width: 1.0),
+                        borderRadius: BorderRadius.all(Radius.circular(36)),
+                      ),
+                      child: Icon(
+                        Icons.favorite,
+                        size: 22,
+                        color: post?.liked == 0
+                            ? Colors.red.withOpacity(0.9)
+                            : Colors.grey.withOpacity(0.8),
+                      ),
+                    ),
+                    clickCallback: () async {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -157,8 +199,6 @@ class PostsPageDetails extends ConsumerWidget {
     return post.files.length > 1
         ? Column(
             children: post?.files?.reversed?.map((file) {
-              print(post?.files?.indexOf(file));
-              print(post?.files?.first == file);
               if (post?.files?.first == file) {
                 return ClipRRect(
                   borderRadius:
@@ -265,7 +305,7 @@ class PostsPageDetails extends ConsumerWidget {
     );
   }
 
-  Positioned _createBackdropFilter(num imageHeight, Post post, Size size) {
+  Widget _createBackdropFilter(num imageHeight, Post post, Size size) {
     return Positioned(
       left: 0,
       right: 0,
@@ -334,10 +374,11 @@ class PostsPageDetails extends ConsumerWidget {
     );
   }
 
-  Positioned _createAppBar(Size size, BuildContext context) {
+  Positioned _createAppBar(Size size, BuildContext context, bool isShowBottomBar) {
     return Positioned(
       top: 20,
-      child: Container(
+      child: AnimatedContainer(
+        duration: duration,
         width: size.width,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -345,16 +386,81 @@ class PostsPageDetails extends ConsumerWidget {
             IconButton(
               iconSize: 17,
               color: Colors.white,
-              icon: Icon(Icons.arrow_back_ios_rounded),
+              icon: Icon(IconFont.icon_back),
               onPressed: () => Navigator.pop(context),
             ),
             IconButton(
-              iconSize: 18,
+              iconSize: 16,
               color: Colors.white,
-              icon: Icon(Icons.more_vert_rounded),
+              icon: Icon(IconFont.icon_moreif),
               onPressed: () {},
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Positioned _createLightAppBar(Size size, BuildContext context,Post post) {
+    StatusBarUtil.setStatusBar(Brightness.dark, color: Colors.white);
+    return Positioned(
+      top: 0,
+      child: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black87.withOpacity(0.1),
+                blurRadius: 8.0,
+                spreadRadius: 1,
+              ),
+            ],
+            color: Colors.white,
+          ),
+          width: size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                iconSize: 17,
+                color: Colors.black87,
+                icon: Icon(IconFont.icon_back),
+                onPressed: () => Navigator.pop(context),
+              ),
+              Expanded(child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipOval(
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: post?.user?.avatar?.mediumAvatarUrl,
+                      fit: BoxFit.cover,
+                      width: 26.0,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 8)),
+                  Text(
+                    post?.user?.name,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black87,
+                      fontFamily: 'SourceHanSans',
+                    ),
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    softWrap: true,
+                  ),
+                ],
+              ),),
+              IconButton(
+                iconSize: 16,
+                color: Colors.black87,
+                icon: Icon(IconFont.icon_moreif),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/status_bar_util.dart
+++ lib/utils/status_bar_util.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class StatusBarUtil {
-  static void setStatusBar(Brightness brightness) {
+  static void setStatusBar(Brightness brightness, {Color color = Colors.transparent}) {
     SystemUiOverlayStyle style = SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
+      statusBarColor: color,
       statusBarIconBrightness: brightness,
     );
     SystemChrome.setSystemUIOverlayStyle(style);
